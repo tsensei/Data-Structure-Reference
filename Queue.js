ed0576d610d2d098a850283262b6b5b3e8187fbd
tsensei <tjsiam019@gmail.com>
2021-08-25 09:10:57 +0600

    Completed Queue

---
+++ Queue.js
@@ -0,0 +1,48 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+
+    this.size++;
+    return this.size;
+  }
+
+  dequeue() {
+    if (!this.first) {
+      return null;
+    }
+    const tempNode = this.first;
+
+    if (this.first === this.last) {
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+
+    this.size--;
+
+    return tempNode.value;
+  }
+}
+
+const queue = new Queue();

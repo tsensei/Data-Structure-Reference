46bcd11f2a305c5e07bfdd650afaff31f0f78697
tsensei <tjsiam019@gmail.com>
2021-08-31 10:50:39 +0600

    Fix Max Binary Heap

--- MaxBinaryHeap.js
+++ MaxBinaryHeap.js
@@ -1,60 +1,77 @@
-class MaxBinaryHeap {
+class BinaryHeap {
   constructor() {
     this.values = [];
   }
 
   insert(value) {
+    if (!value) {
+      throw new Error("Value can't be empty");
+    }
+
     let childIndex = this.values.push(value) - 1;
     let parentIndex = Math.floor((childIndex - 1) / 2);
+    let temp;
 
+    // Loop for swapping if child is greater than parent
+    // while parent is in bound
     while (
-      this.values[parentIndex] < this.values[childIndex] &&
-      parentIndex >= 0
+      parentIndex >= 0 &&
+      this.values[childIndex] > this.values[parentIndex]
     ) {
-      // Swap parent and child value
-      var temp = this.values[parentIndex];
+      temp = this.values[parentIndex];
       this.values[parentIndex] = this.values[childIndex];
       this.values[childIndex] = temp;
 
-      // Make parentIndex the new childIndex
-
+      // parent is the new child as swapped
       childIndex = parentIndex;
       parentIndex = Math.floor((childIndex - 1) / 2);
     }
+
     return this.values;
   }
 
   extractMax() {
     const max = this.values[0];
-    this.values[0] = this.values.pop();
+    const end = this.values.pop();
+    // Edge case if only one node in heap
+    if (!this.values.length) {
+      return max;
+    }
+    this.values[0] = end;
 
-    let parentIndex = 0;
-    let lChildIndex = parentIndex * 2 + 1;
-    let rChildIndex = parentIndex * 2 + 2;
-    let swapIndex, temp;
+    let parentIndex = 0,
+      lChildIndex = 1,
+      rChildIndex = 2;
 
-    while (
-      this.values[parentIndex] < this.values[lChildIndex] ||
-      this.values[parentIndex] < this.values[rChildIndex]
-    ) {
-      if (this.values[lChildIndex] > this.values[rChildIndex]) {
+    while (true) {
+      let swapIndex, temp;
+      if (this.values[lChildIndex] && this.values[rChildIndex]) {
+        if (this.values[lChildIndex] > this.values[rChildIndex]) {
+          swapIndex = lChildIndex;
+        } else {
+          swapIndex = rChildIndex;
+        }
+      } else if (this.values[lChildIndex]) {
         swapIndex = lChildIndex;
       } else {
-        swapIndex = rChildIndex;
+        break;
       }
 
-      //Swap
-      temp = this.values[swapIndex];
-      this.values[swapIndex] = this.values[parentIndex];
-      this.values[parentIndex] = temp;
+      if (this.values[swapIndex] > this.values[parentIndex]) {
+        temp = this.values[swapIndex];
+        this.values[swapIndex] = this.values[parentIndex];
+        this.values[parentIndex] = temp;
 
-      parentIndex = swapIndex;
-      lChildIndex = parentIndex * 2 + 1;
-      rChildIndex = parentIndex * 2 + 2;
+        parentIndex = swapIndex;
+        lChildIndex = parentIndex * 2 + 1;
+        rChildIndex = parentIndex * 2 + 2;
+      } else {
+        break;
+      }
     }
 
     return max;
   }
 }
 
-const heap = new MaxBinaryHeap();
+let heap = new BinaryHeap();

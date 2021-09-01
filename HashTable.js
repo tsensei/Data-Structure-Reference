e439e26926368f016e714f9551a2113f20a46538
tsensei <tjsiam019@gmail.com>
2021-09-01 12:34:30 +0600

    Completed Hash Table

---
+++ HashTable.js
@@ -0,0 +1,34 @@
+class HashTable {
+  constructor(size = 4) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+
+  set(key, value) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+
+    this.keyMap[index].push([key, value]);
+  }
+
+  get(key) {
+    let index = this._hash(key);
+    let array = this.keyMap[index];
+    let item = array.find((item) => item[0] === key);
+    return item ? item[1] : undefined;
+  }
+}
+
+let ht = new HashTable();
